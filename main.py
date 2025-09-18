bb90cf4ed2f74fb4383fbb9b7153ea5894b1b448
kevin61517 <kevin61517@yahoo.com.tw>
2025-09-18 12:08:47 +0800

    測試文件調整為參數化測試

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import asyncio
 
-from src.infra.engine.playwright import Playwright
-from src.infra.engine import engine_factory
+from src.infra.engine import engine_factory, Engine
 
 
 async def main():
-    engine = await engine_factory('Playwright')
-    browser = await engine.init_browser('chromium')
-    print('browser---->', browser)
-    async with await browser.launch(headless=False) as b:
+    # 設置測試參數
+    engine_name = 'Playwright'  # 引擎
+    browser_name = 'Chromium'  # 瀏覽器
+    url = 'https://google.com'  # 頁面
+    headless = False
+
+    # 初始化測試物件
+    engine = await engine_factory(Engine, engine_name)
+    browser = await engine.init_browser(browser_name)
+
+    # 開始測試
+    async with await browser.launch(headless=headless) as b:
         page = await b.new_page()
-        print(page)
-        await page.goto('https://google.com')
+        await page.goto(url)
         print(await page.title())
+        await asyncio.sleep(3)
 
 
 if __name__ == '__main__':
     asyncio.run(main())
-    # print(dir(a))
